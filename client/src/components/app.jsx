c095f63e8d30f1c1040f0eeb91ec0f43f082ac7c
Michael Wetterauer <mjw0486@gmail.com>
2020-11-13 18:11:38 -0500

    Changed api get path in server to only get the requested product id. Added s3 urls. Created basic axios req to confirm urls render properly. Added seeding script, used faker for data and s3 for images.

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,15 +1,50 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      requestedProductId: '4',
+      productId: '',
+      productDetails: {
+        designer: '',
+        materials: [],
+        care: [],
+        Packaging: {
+          packagingName: '',
+          packagingDescription: '',
+          width: '',
+          height: '',
+          length: '',
+          weight: '',
+          packages: ''
+        }
+      },
+      images: []
     }
   }
+
+  componentDidMount() {
+    axios.get(`/api/products/${this.state.requestedProductId}`)
+      .then((response) => {
+        this.setState({productId: response.data.productId})
+        this.setState({productDetails: response.data.productDetails})
+        this.setState({images: response.data.images})
+      })
+      .catch(function(err){
+        console.log(err);
+      });
+  }
+
   render() {
     return(
-      <div>React is connected</div>
+      <div>
+        <img src={this.state.images[0]}>
+        </img>
+        {this.state.images}
+      </div>
+
     )
   }
 }
