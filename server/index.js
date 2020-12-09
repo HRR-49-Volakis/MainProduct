ba02fa6a700df2d3f6e22d797abd471aa9b3014b
giovanirubini <giovanirubinimaccagno@gmail.com>
2020-12-09 18:08:06 -0500

    cleaning

--- server/index.js
+++ server/index.js
@@ -1,36 +1,6 @@
-const path = require('path');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const express = require('express');
-const routes = require('./routes');
-// const { getInstance } = require('../database/mongo');
-// const { getInstance, getProducts } = require('../database/postgress');
-const { cassandra, mongo, postgress } = require('../database');
+const app = require('./server.js');
+const port = 3001;
 
-const app = express();
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../public')));
-
-app.get('/hey', (req, res, next) => {
-  getProducts()
-    .then(result => result.rows)
-    .then(rows => console.log(rows))
-    .catch(e => {
-      console.log('the error is ', e);
-    });
+app.listen(port, function() {
+  console.log(`listening on port ${port}`);
 });
-
-app.post('/api/main', async function(req, res) {
-  let response = await productsDB.ProductModel.create(req.body);
-  res.status(200).send(response);
-});
-
-app.use('/api/product', routes.ProductRoutes);
-app.use('/api/user', routes.UserRoutes);
-app.use('/db', routes.DBRoutes);
-app.use('/', routes.WebRoutes);
-
-module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,19 @@
-const app = require('./index.js');
-const port = 3001;
+const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const express = require('express');
+const routes = require('./routes');
 
-app.listen(port, function() {
-  console.log(`listening on port ${port}`);
-});
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use('/api/product', routes.ProductRoutes);
+app.use('/api/user', routes.UserRoutes);
+app.use('/db', routes.DBRoutes);
+app.use('/', routes.WebRoutes);
+
+module.exports = app;
